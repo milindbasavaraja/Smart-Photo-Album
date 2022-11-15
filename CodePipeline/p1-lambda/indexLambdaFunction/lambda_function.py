a697d70e932addf1c912a5fac799a9cbe31e6983
milindbasavaraja <milindbasavaraja96@gmail.com>
2022-11-14 19:37:06 -0500

    Update lambda_function.py

--- CodePipeline/p1-lambda/indexLambdaFunction/lambda_function.py
+++ CodePipeline/p1-lambda/indexLambdaFunction/lambda_function.py
@@ -91,5 +91,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Thanks Come Again!')
     }
